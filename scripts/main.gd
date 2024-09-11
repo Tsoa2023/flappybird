40b309fa91fb9b4acb0d31828b1c57293628521d
m2cky <faellafdr@gmail.com>
2024-09-11 07:33:40 +0300

    add restart

--- scripts/main.gd
+++ scripts/main.gd
@@ -21,15 +21,14 @@ func new_game():
 	game_running = false
 	game_over = false
 	score = 0
+	$ScoreLabel.text = "SCORE: " + str(score)
+	$GameOver.hide()
+	get_tree().call_group("pipes", "queue_free")
+	pipe_array.clear()
 	scroll = 0
-	reset_pipes()
 	create_new_pipe()
 	$Bird.reset()
 	
-func reset_pipes():
-	for pipe in pipe_array:
-		pipe.queue_free()
-	pipe_array.clear()
 
 func _input(event):
 	if game_over == false:
@@ -48,26 +47,41 @@ func start_game():
 	$Timer.start()
 
 func create_new_pipe():
+	var pipes_to_remove = []
+	for pipe in pipe_array:
+		if is_instance_valid(pipe) and pipe.position.x < -100:
+			pipes_to_remove.append(pipe)
+	for pipe in pipes_to_remove:
+		remove_pipe(pipe)	 
 	new_pipe = pipe.instantiate();
 	new_pipe.position.x = 400
 	new_pipe.position.y = coord[randi() % 5]
 	new_pipe.hit.connect(bird_hit)
+	new_pipe.scored.connect(scored)
 	add_child(new_pipe)
 	pipe_array.append(new_pipe)
 	
+func remove_pipe(pipe):
+	if pipe in pipe_array:
+		pipe_array.erase(pipe)
+	pipe.queue_free()
+	
+func scored():
+	score  +=  1
+	$ScoreLabel.text = "SCORE: " + str(score)
 func check_top():
 	if $Bird.position.y < 0:
 		$Bird.falling = true
 		stop_game()
 
 func bird_hit():
+	$Bird.falling = true
 	stop_game()
 
 func stop_game():
 	$Timer.stop()
 	$Bird.flying = false
-	
-	$Bird.falling = true
+	$GameOver.show()
 	game_running = false
 	game_over = true
 	print("GAME OVER\n")
@@ -78,14 +92,38 @@ func stop_game():
 	
 func _process(delta: float) -> void:
 	if game_running:
+		var pipes_to_remove = []
 		for pipe in pipe_array:
-			pipe.position.x -= SCROLL_SPEED
+			if is_instance_valid(pipe):
+				pipe.position.x -= SCROLL_SPEED
+			else:
+				pipes_to_remove.append(pipe)
+		
+		# Supprimez les tuyaux invalides du tableau
+		for pipe in pipes_to_remove:
+			pipe_array.erase(pipe)
+
 		scroll += SCROLL_SPEED
 		if scroll >= screen_size.x:
 			scroll = 0
 		$Ground.position.x = -scroll
 		$Ground2.position.x = -scroll + screen_size.x
-		
+			
 
 func _on_timer_timeout() -> void:
 	create_new_pipe();
+
+func _on_ground_hit() -> void:
+	$Bird.falling = true
+	$Bird.velocity = Vector2.ZERO
+	stop_game()
+	
+
+func _on_ground_2_hit() -> void:
+	$Bird.falling = true
+	$Bird.velocity = Vector2.ZERO
+	stop_game()
+
+
+func _on_game_over_restart() -> void:
+	new_game()
--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 signal hit
+signal scored
 
 
 func _on_body_entered(body: Node2D) -> void:
 	hit.emit()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	scored.emit()
